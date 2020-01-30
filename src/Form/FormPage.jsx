974429e79a5141d4c227afd2113295f69115619d
MoMentalochka <56727108+MoMentalochka@users.noreply.github.com>
2020-01-31 02:20:47 +0300

    Form

---
+++ src/Form/FormPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./FormPage.module.css"
+import Form from './Form'
+const FormPage = () => {
+    return (
+        <div className={styles.container}>
+            <Form/>
+
+        </div>
+    )
+}
+export default FormPage
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './bootstrap.css';
+import './font-awesome-4.7.0/font-awesome-4.7.0/css/font-awesome.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
