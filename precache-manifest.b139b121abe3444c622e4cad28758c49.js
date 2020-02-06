1b9299b6becd9c156727c379aed353c48b43d9c5
MoMentalochka <56727108+MoMentalochka@users.noreply.github.com>
2020-02-06 16:37:54 +0300

    Updates

---
+++ precache-manifest.b139b121abe3444c622e4cad28758c49.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c6e2d0816cc2a2b0c8fd1dd9b2fc6b4",
+    "url": "/MessageForm/index.html"
+  },
+  {
+    "revision": "fbe7c91ebbc5b33c284d",
+    "url": "/MessageForm/static/css/main.c5e43867.chunk.css"
+  },
+  {
+    "revision": "a06b5bfb5e4a9b280449",
+    "url": "/MessageForm/static/js/2.44a6b682.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/MessageForm/static/js/2.44a6b682.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fbe7c91ebbc5b33c284d",
+    "url": "/MessageForm/static/js/main.3be36738.chunk.js"
+  },
+  {
+    "revision": "84b57dfbe45ca4161a8d",
+    "url": "/MessageForm/static/js/runtime-main.1c730a31.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/MessageForm/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/MessageForm/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/MessageForm/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/MessageForm/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/MessageForm/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fe532a17289c6871607e46687f6849e3.js"
+  "/MessageForm/precache-manifest.b139b121abe3444c622e4cad28758c49.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MessageForm/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
