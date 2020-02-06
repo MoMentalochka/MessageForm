f980856d0e9325458f4c0992599256f5d447e317
MoMentalochka <56727108+MoMentalochka@users.noreply.github.com>
2020-02-06 16:35:09 +0300

    Updates

---
+++ precache-manifest.fe532a17289c6871607e46687f6849e3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6b5ec6d351607a873c02574e0d7b1c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60fde320457d45b9548f",
+    "url": "/static/css/main.01fc1c81.chunk.css"
+  },
+  {
+    "revision": "a06b5bfb5e4a9b280449",
+    "url": "/static/js/2.44a6b682.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.44a6b682.chunk.js.LICENSE"
+  },
+  {
+    "revision": "60fde320457d45b9548f",
+    "url": "/static/js/main.adbf0ae8.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
